908546217941dc8a33dcbb4e8d8a9b7f9b71fc8e
David Medel <dmedel.ba@gmail.com>
2020-10-15 02:45:07 -0300

    up

--- cliente.go
+++ cliente.go
@@ -1,20 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 
 // Package main implements a client for Greeter service.
 package main
@@ -26,12 +9,11 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
-	address     = "localhost:50051"
-	defaultName = "world"
+	address     = "dist69:6969"
+	defaultName = "caca"
 )
 
 func main() {
@@ -41,7 +23,7 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+
 
 	// Contact the server and print out its response.
 	name := defaultName
@@ -50,9 +32,6 @@ func main() {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	
+	log.Printf("Greeting: HOLAAAAA MACA")
 }
