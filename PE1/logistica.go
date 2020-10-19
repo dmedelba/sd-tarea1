52986a446f6a12f8a4b28e9f02ead6ae27cc3be8
David Medel <dmedel.ba@gmail.com>
2020-10-19 00:08:12 -0300

    lazy push

--- PE1/logistica.go
+++ PE1/logistica.go
@@ -106,7 +106,7 @@ func guardarPaquetesLogisticaRT(in *pb.SolicitudPedidoRetail, codigoSeguimiento 
 //solicitar el estado de un pedido
 func ObtenerCodigoSeguimiento(ctx context.Context, in *pb.SolicitudSeguimiento) (*pb.RespuestaSeguimiento, error){
 	log.Printf("[Servidor] Consultando el codigo: %d", in.CodigoSeguimiento)
-	return &pb.RespuestaSeguimiento{EstadoPedido:"okokokokok"}
+	return &pb.RespuestaSeguimiento{EstadoPedido:"okokokokok"}, nil
 }
 
 //recibir pedidoPyme
